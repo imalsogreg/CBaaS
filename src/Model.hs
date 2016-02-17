3535662a1427d892230cb2ccc6c7d88f44ab5c56
Greg Hale <imalsogreg@gmail.com>
2016-02-16 23:46:34 -0500

    get real testsuite up for serializing vals

--- src/Model.hs
+++ src/Model.hs
@@ -1,22 +1,28 @@
 {-# LANGUAGE GADTs #-}
 {-# LANGUAGE DataKinds #-}
 {-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE TemplateHaskell #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
 {-# LANGUAGE StandaloneDeriving #-}
 
 module Model where
 
 import Control.Applicative ((<|>))
+import Control.DeepSeq
 import Control.Monad (mzero)
 import Data.ByteString
 import qualified Data.ByteString.Lazy as BL
 import qualified Data.ByteString.Base64 as B64
+import Data.Complex
 import Data.Text
 import qualified Data.Text.Lazy as TL
 import Data.Text.Encoding
 import qualified Data.Aeson as A
 import qualified Data.Vector as V
+import Generics.SOP
+import Generics.SOP.NFData
 import GHC.Generics
 import GHC.TypeLits
 import Codec.Picture
@@ -54,6 +60,8 @@ class FromVal a where
 data Expr = Expr A.Value
   deriving (Eq, Show)
 
+instance NFData Expr where
+  rnf (Expr v) = rnf v
 
 instance A.ToJSON Expr where
   toJSON (Expr v) = v
@@ -62,57 +70,103 @@ instance A.FromJSON Expr where
   parseJSON (A.Object v) = pure (Expr $ A.Object v)
   parseJSON _            = mzero
 
-data Val = VAny    A.Value
-         | VDouble Double
-         | VText   Text
-         | VList   [Val]
-         | VProbabilityDistribution [(Val,Double)]
-         | VVec1   (V.Vector Double)
-         | VVec2   (V.Vector (Double, Double))
-         | VVec3   (V.Vector (Double, Double, Double))
-         | VMat2   (V.Vector (V.Vector Double))
-         | VMat2C  (V.Vector (V.Vector Double),
-                    V.Vector (V.Vector Double),
-                    V.Vector (V.Vector Double))
-         | VImage  ModelImage
-         | VClosure [(Text,Val)] Expr
-         deriving (Eq, Show, Generic)
+data Val = -- VAny    A.Value
+        --  |
+           VPrim PrimVal
+         | VList   [PrimVal]
+         -- | VProbabilityDistribution [(Val,Double)]
+         | VVec1   (V.Vector PrimVal)
+         | VVec2   (V.Vector (PrimVal, PrimVal))
+         | VVec3   (V.Vector (PrimVal, PrimVal, PrimVal))
+         | VMat2   (V.Vector (V.Vector PrimVal))
+         | VMat2C  (V.Vector (V.Vector PrimVal),
+                    V.Vector (V.Vector PrimVal),
+                    V.Vector (V.Vector PrimVal))
+         -- | VClosure [(Text,Expr)] Expr
+         deriving (Eq, Show, GHC.Generics.Generic)
 
+data PrimVal = PrimDouble Double
+             | PrimComplex PrimComplex
+             | PrimText   Text
+             | PrimImage  ModelImage
+  deriving (Eq, Show, GHC.Generics.Generic)
 
-instance A.ToJSON Val where
-  toJSON (VImage (ModelImage dynImg)) =
-    let bytes = case encodeModelImage (ModelImage dynImg) of
-          Left e -> "Encoding Error"
-          Right b -> b
-    in A.object ["tag" A..= ("VImage" :: Text)
-                ,"contents" A..= bytes]
-  toJSON (VDouble d) = A.object ["tag" A..= ("VDouble" :: Text)
-                                ,"contents" A..= d]
-  toJSON (VText t) = A.object ["tag" A..= ("VText" :: Text)
-                              ,"contents" A..= t]
-  toJSON (VMat2 m) = A.object ["tag" A..= ("VMat2" :: Text)
-                              ,"contents" A..= m]
-  toJSON (VMat2C (r,g,b)) = A.object ["tag" A..= ("VMat2C" :: Text)
-                                     ,"contents" A..= (r,g,b)]
-  toJSON (VAny v) = v
+instance Generics.SOP.Generic PrimVal
 
-instance A.FromJSON Val where
-  parseJSON (A.Object o) = (do
-    tag :: String <- o A..: "tag"
-    contents      <- o A..: "contents"
-    case tag of
-      "VImage" -> case decodeModelImage contents of
-        Left _    -> mzero
-        Right img -> return (VImage img)
-      "VDouble" -> VDouble <$> A.parseJSON contents
-      "VText"   -> VText   <$> A.parseJSON contents
-      "VMat2"   -> VMat2   <$> A.parseJSON contents
-      "VMat2C"  -> VMat2C  <$> A.parseJSON contents
-      "VAny"    -> return $ VAny contents
-    ) -- <|> pure (VAny (A.Object o))
-  -- parseJSON a = return $ VAny a
+instance Generics.SOP.Generic Val
+
+newtype PrimComplex = PComplex { getComplex :: Complex Double }
+  deriving (Eq, Show, GHC.Generics.Generic, NFData)
+
+instance A.ToJSON PrimComplex where
+  toJSON (PComplex c) = A.object ["real" A..= realPart c
+                                 ,"imag" A..= imagPart c]
+
+instance A.FromJSON PrimComplex where
+  parseJSON (A.Object o) = do
+    r <- o A..: "real"
+    i <- o A..: "imag"
+    return (PComplex (r :+ i))
+  parseJSON _ = mzero
+
+instance NFData PrimVal where
+  rnf = grnf
+
+instance NFData Val where
+  rnf = grnf
+
+instance A.ToJSON PrimVal
+
+instance A.ToJSON Val
+
+-- instance A.ToJSON Val where
+--   toJSON (VAny v) = v
+--   toJSON (VDouble d) = A.object ["tag" A..= ("VDouble" :: Text)]
+--   toJSON (VText t) = A.object ["tag" A..= ("VText" :: Text)
+--                               ,"contents" A..= t]
+--                               ,"contents" A..= d]
+
+--   toJSON (VImage (ModelImage dynImg)) =
+--     let bytes = case encodeModelImage (ModelImage dynImg) of
+--           Left e -> "Encoding Error"
+--           Right b -> b
+--     in A.object ["tag" A..= ("VImage" :: Text)
+--                 ,"contents" A..= bytes]
+--   toJSON (VMat2 m) = A.object ["tag" A..= ("VMat2" :: Text)
+--                               ,"contents" A..= m]
+--   toJSON (VMat2C (r,g,b)) = A.object ["tag" A..= ("VMat2C" :: Text)
+--                                      ,"contents" A..= (r,g,b)]
+
+instance A.FromJSON PrimVal
+instance A.FromJSON Val
+
+-- instance A.FromJSON Val where
+--   parseJSON (A.Object o) = (do
+--     tag :: String <- o A..: "tag"
+--     contents      <- o A..: "contents"
+--     case tag of
+--       "VImage" -> case decodeModelImage contents of
+--         Left _    -> mzero
+--         Right img -> return (VImage img)
+--       "VDouble" -> VDouble <$> A.parseJSON contents
+--       "VText"   -> VText   <$> A.parseJSON contents
+--       "VMat2"   -> VMat2   <$> A.parseJSON contents
+--       "VMat2C"  -> VMat2C  <$> A.parseJSON contents
+--       "VAny"    -> return $ VAny contents
+--     ) -- <|> pure (VAny (A.Object o))
+--   -- parseJSON a = return $ VAny a
 
 newtype ModelImage = ModelImage DynamicImage
+
+instance A.ToJSON ModelImage where
+  toJSON i = case encodeModelImage i of
+    Left e  -> A.Null
+    Right v -> v
+
+instance A.FromJSON ModelImage where
+  parseJSON v = case decodeModelImage v of
+    Left e  -> mzero
+    Right v -> return v
 
 instance Eq ModelImage where
   a == b =
@@ -121,37 +175,40 @@ instance Eq ModelImage where
 instance Show ModelImage where
   show _ = "<CBaaSModelImage>"
 
+instance NFData ModelImage where
+  rnf (ModelImage img) = rnf img
+
 encodeModelImage :: ModelImage -> Either String A.Value
 encodeModelImage (ModelImage img) =
-  (A.String . decodeUtf8 . B64.encode . BL.toStrict) <$> encodeDynamicPng img
+  (A.String . decodeUtf8 . B64.encode . BL.toStrict)
+    <$> encodeDynamicPng img
 
 decodeModelImage :: A.Value -> Either String ModelImage
 decodeModelImage (A.String (s :: Text)) =
   fmap ModelImage $ decodeImage =<< B64.decode (encodeUtf8 s)
 
 
-instance Model.ToVal Int where
-  toVal i = Model.VDouble (realToFrac i)
+-- instance Model.ToVal Int where
+--   toVal i = Model.VDouble (realToFrac i)
 
-instance Model.ToVal Double where
-  toVal = VDouble
+-- instance Model.ToVal Double where
+--   toVal = VDouble
 
-instance Model.ToVal a => Model.ToVal [a] where
-  toVal i = Model.VList $ Prelude.map toVal i
+-- instance Model.ToVal a => Model.ToVal [a] where
+--   toVal i = Model.VList $ Prelude.map toVal i
 
-instance Model.ToVal Text where
-  toVal = VText
+-- instance Model.ToVal Text where
+--   toVal = VText
 
 
-instance Model.FromVal Double where
-  fromVal (VDouble t) = t
-  fromVal e = error $ "Couldn't cast to double: " ++ show e
+-- instance Model.FromVal Double where
+--   fromVal (VDouble t) = t
+--   fromVal e = error $ "Couldn't cast to double: " ++ show e
 
-instance Model.FromVal Text where
-  fromVal (VText t) = t
-  fromVal e = error $ "Couldn't cast to text: " ++ show e
+-- instance Model.FromVal Text where
+--   fromVal (VText t) = t
+--   fromVal e = error $ "Couldn't cast to text: " ++ show e
 
-instance Model.FromVal DynamicImage where
-  fromVal (Model.VImage (Model.ModelImage i)) = i
-  fromVal x = error $ "Couldn't cast to image: " ++ show x
-
+-- instance Model.FromVal DynamicImage where
+--   fromVal (Model.VImage (Model.ModelImage i)) = i
+--   fromVal x = error $ "Couldn't cast to image: " ++ show x
--- test/ModelSpec.hs
+++ test/ModelSpec.hs
@@ -1,23 +1,138 @@
 {-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE ScopedTypeVariables #-}
 
 module ModelSpec where
 
+import Control.Applicative
+import Codec.Picture
+import qualified Data.Aeson as A
+import Data.Complex
+import qualified Data.HashMap.Strict as HM
+import Data.Proxy
+import qualified Data.Text as T
+import qualified Data.Text.Lazy as TL
+import qualified Data.Vector as V
 import GHC.Generics
+import GHC.Word
 import Generics.SOP.Arbitrary
 import Generics.SOP
 import Test.QuickCheck
+import Test.QuickCheck.Function
 import Test.QuickCheck.Arbitrary
 import Test.Hspec
+import Test.Hspec.QuickCheck
+import Model
 
 spec = do
-  describe "Simple test" $ do
-    it "is seen by hspec" $ do
-      True `shouldBe` True
+  describe "Encoding and decoding"
+    $ modifyMaxSize (const 3)
+    $ modifyMaxSuccess (const 10) $ do
+      it "primval should round-trip aeson" $ property $ \(pv :: PrimVal) ->
+        A.decode (A.encode pv) == Just pv
+      it "val should round-trip aeson" $  property $ \(v :: Val) ->
+        A.decode (A.encode v) == Just v
+
+instance Arbitrary PrimVal where
+  arbitrary = garbitrary
+
+instance Arbitrary Val where
+  arbitrary = garbitrary
+
+instance Arbitrary PrimComplex where
+  arbitrary = fmap PComplex $ liftA2 (:+) arbitrary arbitrary
+
+instance Arbitrary A.Value where
+  arbitrary = oneof
+    [ fmap (A.String . T.pack) arbitrary
+    , fmap (A.Number . realToFrac) (arbitrary :: Gen Double)
+    , return A.Null
+    , fmap (A.Object . HM.fromList) (zip <$> arbitrary <*> arbitrary)
+    , fmap (A.Array . V.fromList) arbitrary
+    ]
+
+instance Arbitrary Expr where
+  arbitrary = fmap Expr arbitrary
+
+instance Arbitrary T.Text where
+  arbitrary = T.pack <$> arbitrary
+
+genImg :: (Pixel a, Arbitrary a)
+       => Proxy a
+       -> Int
+       -> Int
+       -> Gen (Image a)
+genImg _ wid hgt = do
+  f :: Fun (Int,Int) a <- arbitrary
+  let img = generateImage (curry (apply f)) wid hgt
+  return img
+
+r16 :: (Word16,Word16)
+r16 = (minBound,maxBound)
+
+r8 :: (Word8,Word8)
+r8 = (minBound,maxBound)
+
+rF :: (Float, Float)
+rF = (0,1)
+
+instance Arbitrary a => Arbitrary (V.Vector a) where
+  arbitrary = fmap V.fromList arbitrary
+
+instance Arbitrary PixelYA8 where
+  arbitrary = liftA2 PixelYA8 (choose r8) (choose r8)
+
+instance Arbitrary PixelYA16 where
+  arbitrary = liftA2 PixelYA16 (choose r16) (choose r16)
+
+instance Arbitrary PixelRGB8 where
+  arbitrary = liftA3 PixelRGB8 (choose r8) (choose r8) (choose r8)
+
+instance Arbitrary PixelRGB16 where
+  arbitrary = liftA3 PixelRGB16 (choose r16) (choose r16) (choose r16)
+
+instance Arbitrary PixelRGBF where
+  arbitrary = liftA3 PixelRGBF (choose rF) (choose rF) (choose rF)
+
+liftA4 :: Applicative m
+       => (a -> b -> c -> d -> e) -> m a -> m b -> m c -> m d -> m e
+liftA4 f a b c d = f <$> a <*> b <*> c <*> d
+
+instance Arbitrary PixelRGBA8 where
+  arbitrary = liftA4 PixelRGBA8 (choose r8) (choose r8) (choose r8) (choose r8)
+
+instance Arbitrary PixelRGBA16 where
+  arbitrary = liftA4 PixelRGBA16 (choose r16) (choose r16) (choose r16) (choose r16)
+
+instance Arbitrary PixelCMYK8 where
+  arbitrary = liftA4 PixelCMYK8 (choose r8) (choose r8) (choose r8) (choose r8)
+
+instance Arbitrary PixelCMYK16 where
+  arbitrary = liftA4 PixelCMYK16 (choose r16) (choose r16) (choose r16) (choose r16)
+
+instance Arbitrary PixelYCbCr8 where
+  arbitrary = liftA3 PixelYCbCr8 (choose r8) (choose r8) (choose r8)
+
 
 instance Arbitrary ModelImage where
-  arbitrary = do
-    xSize <- condition (< 2000) arbitrary
-    ySize <- condition (< 2000) arbitrary
-    fmt <- choose [Jpeg, Png, Gif, BMP]
-    case fmt of
-      Jpeg -> something
+  arbitrary = genModelImage
+
+genModelImage :: Gen ModelImage
+genModelImage = do
+    x <- choose (10 :: Int, 200)
+    y <- choose (10 :: Int, 200)
+    dynImg <- oneof
+      [ -- ImageY8     <$> genImg (Proxy :: Proxy Pixel8)      x y
+      -- , ImageY16    <$> genImg (Proxy :: Proxy Pixel16)     x y
+      -- , ImageYF     <$> genImg (Proxy :: Proxy PixelF)      x y
+      -- , ImageYA8    <$> genImg (Proxy :: Proxy PixelYA8)    x y
+      -- , ImageYA16   <$> genImg (Proxy :: Proxy PixelYA16)   x y
+        ImageRGB8   <$> genImg (Proxy :: Proxy PixelRGB8)   x y
+      , ImageRGB16  <$> genImg (Proxy :: Proxy PixelRGB16)  x y
+      -- , ImageRGBF   <$> genImg (Proxy :: Proxy PixelRGBF)   x y
+      , ImageRGBA8  <$> genImg (Proxy :: Proxy PixelRGBA8)  x y
+      , ImageRGBA16 <$> genImg (Proxy :: Proxy PixelRGBA16) x y
+      -- , ImageYCbCr8 <$> genImg (Proxy :: Proxy PixelYCbCr8) x y
+      -- , ImageCMYK8  <$> genImg (Proxy :: Proxy PixelCMYK8)  x y
+      -- , ImageCMYK16 <$> genImg (Proxy :: Proxy PixelCMYK16) x y
+      ]
+    return (ModelImage dynImg)
