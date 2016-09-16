39ea68aaea71e87817fb1f1ed92f11ca392fbc5b
Greg Hale <imalsogreg@gmail.com>
2016-09-16 15:02:16 -0400

    Fix ghc overrides on frontend

--- cbaas-frontend/ghc.nix
+++ cbaas-frontend/ghc.nix
@@ -13,5 +13,11 @@ reflex-platform.ghc.override {
      groundhog-ghcjs      = dontCheck (self.callPackage (cabal2nixResult ../deps/groundhog-ghcjs) {});
      groundhog-postgresql = null;
      cbaas-lib            = dontCheck (self.callPackage ../cbaas-lib {  });
+     http-api-data        = dontCheck (self.callPackage (cabal2nixResult ../deps/http-api-data) {  });
+     servant        = dontCheck (self.callPackage (cabal2nixResult ../deps/servant/servant) {  });
+     servant-js        = dontCheck (self.callPackage (cabal2nixResult ../deps/servant/servant-js) {  });
+     servant-foreign        = dontCheck (self.callPackage (cabal2nixResult ../deps/servant/servant-foreign) {  });
+     servant-docs        = dontCheck (self.callPackage (cabal2nixResult ../deps/servant/servant-docs) {  });
+
   };
 }
